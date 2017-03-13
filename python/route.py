2d66862faa86de1ea69abf5a1f3c2e0d79ffed8f
bakowroc <bakowroc@gmail.com>
2017-03-13 12:23:03 +0100

    Remove unnecessary route provider

--- python/route.py
+++ python/route.py
@@ -5,12 +5,5 @@ APP = Flask(__name__)
 @APP.route('/')
 def index():
     return "Hello World!"
-
-@APP.route('/profile/<zmienna>')
-def profile(zmienna):
-    if zmienna is not '0':
-        return "nazywam sie {0}".format(zmienna)
-    else: return redirect('/')
-
 if __name__ =="__main__":
     APP.run(debug=True)

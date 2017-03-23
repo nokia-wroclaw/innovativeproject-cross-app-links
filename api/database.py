2487c712336d1bc0d5aff2111678c4e2ed72a8ff
bakowroc <kontakt@maciejbakowicz.pl>
2017-03-23 23:20:35 +0100

    Database setup

--- api/database.py
+++ api/database.py
@@ -1,1 +1,11 @@
 from api import app
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+
+#app.config['SQLALCHEMY_DATABASE_URI']= os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://thntcklaiyqbpp:f6f54577ecd3ef055e00ba7175b579a0726d018545e53742d3c1f21c6da236ee@ec2-54-247-166-129.eu-west-1.compute.amazonaws.com:5432/d9nu6m5q6271on'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db = SQLAlchemy(app)

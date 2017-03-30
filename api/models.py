470875fb22417c20fff8d7c05b632a6f1b4d65ec
bakowroc <kontakt@maciejbakowicz.pl>
2017-03-30 22:50:25 +0200

    Fix models and relationships

--- api/models.py
+++ api/models.py
@@ -1,61 +1,64 @@
 from flask_login import UserMixin
 from api.database import db
 
+class User(UserMixin, db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(25), unique=True)
+    username = db.Column(db.String(30), unique=True)
+    password_hash =db.Column(db.String(128))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    
+    applications = db.relationship('App', backref='creator', lazy='dynamic')
+    logs = db.relationship('Log', backref='author', lazy='dynamic')
 
-rel = db.Table('rel',
-                   db.Column('user_id', db.Integer, db.ForeignKey('user.user_id')),
-                   db.Column('group_id', db.Integer, db.ForeignKey('group.group_id'))
-                   )
-rel1 = db.Table('rel1',
-                    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id')),
-                    db.Column('app_id', db.Integer, db.ForeignKey('app.app_id')))
-
-
-
-class User(UserMixin,db.Model):
-        user_id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(30), unique=True)
-        relations = db.relationship('Group', secondary=rel, backref=db.backref('group_members', lazy='dynamic'))
-        relations1 = db.relationship('App', secondary=rel1, backref=db.backref('app_owners', lazy='dynamic'))
-
-        def __init__(self,user_id,username):
-            self.user_id=user_id
-            self.username=username
+    def __init__(self, email, password_hash):
+        
+        self.email = email
+        self.password_hash = password_hash
+        self.username = email.split('@')[0]
 
             
             
 class Group(db.Model):
-        group_id = db.Column(db.Integer, primary_key=True)
-        group_name = db.Column(db.String(30), unique=True)
-
-        def __init__(self,group_id,group_name):
-            self.group_id=group_id
-            self.group_name=group_name
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    
+    users = db.relationship('User', backref='group', lazy='dynamic')
+        
+    def __init__(name):
+        
+        self.group_name = name
 
             
             
 class App(db.Model):
-        app_id = db.Column(db.Integer, primary_key=True)
-        app_name = db.Column(db.String(30), unique=True)
-        app_link = db.Column(db.String(50), unique=True)
-        app_desc = db.Column(db.String(50), unique=True)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    link = db.Column(db.String(50), unique=True)
+    desc = db.Column(db.String(50))
+    creator_id = db.Column(dbInteger, db.ForeignKey('user.id'))
 
-        def __init__(self,app_id,app_name,app_link,app_desc):
-            self.app_id=app_id
-            self.app_name=app_name
-            self.app_link=app_link
-            self.app_desc=app_desc
+    def __init__(self, name, link, desc, creator_id):
+        
+        self.name = name
+        self.link = link
+        self.desc = desc
+        self.creator_id = creator_id
             
             
             
-class Log   (db.Model):
-    log_id = db.Column(db.Integer,primary_key=True)
-    data_time = db.Column(db.TIMESTAMP,unique=True)
-    author = db.Column(db.String(40),unique=True)
+class Log(db.Model):
+    
+    id = db.Column(db.Integer,primary_key=True)
+    data_time = db.Column(db.TIMESTAMP)
     content = db.Column(db.String(60),unique=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self,log_id,data_time,author,content):
-        self.log_id=log_id
-        self.data_time=data_time
-        self.author=author
-        self.content=content
+    def __init__(self, data_time, content, author_id):
+        
+        self.data_time = data_time
+        self.author = author
+        self.content = content

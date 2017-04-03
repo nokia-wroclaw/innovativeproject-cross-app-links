ab710697e69353ee7a74b495f3df2770bb1cf1d8
malwina-zeglen <malwina.zeglen@gmail.com>
2017-04-03 18:09:21 +0200

    link-management form validation

--- api/static/js/directives.js
+++ api/static/js/directives.js
@@ -1,11 +1,11 @@
 (function () {
     var app = angular.module("directives", []);
-    app.directive('linkFormat', function () {
+    app.directive('linkRequired', function () {
         return {
             require: 'ngModel',
             link: function (scope, element, attr, mCtrl) {
                 function linkvalidate(value) {
-                    if (/^(http\:\/\/www+\.+[a-z0-9]+\.+[a-z0-9])/.test(value)) {
+                    if (/^(http\:\/\/www+\.+[a-z0-9]+\.+[a-z0-9])$/.test(value)) {
                         mCtrl.$setValidity('linkformat', true);
                     } else {
                         mCtrl.$setValidity('linkformat', false);

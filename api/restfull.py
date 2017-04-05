da154263acecd25e58a08b42c6c5ae696f0e08c9
bakowroc <kontakt@maciejbakowicz.pl>
2017-04-05 16:08:47 +0200

    Change results per page to inf (api)

--- api/restfull.py
+++ api/restfull.py
@@ -13,17 +13,17 @@ def auth_func(*args, **kw):
 manager = APIManager(app, flask_sqlalchemy_db=db, preprocessors=dict(POST=[auth_func], DELETE=[auth_func], PUT=[auth_func]))
 
 # /api/user , /api/user/<int>
-manager.create_api(User,  exclude_columns=['password_hash'], methods=['GET', 'POST', 'DELETE','PUT'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+manager.create_api(User,  exclude_columns=['password_hash'], methods=['GET', 'POST', 'DELETE','PUT'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]), results_per_page=0)
 
 # /api/group , /api/group/<int>
-manager.create_api(Group, exclude_columns=['users.password_hash', 'users.group_id'], methods=['GET', 'POST', 'DELETE','PUT'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+manager.create_api(Group, exclude_columns=['users.password_hash', 'users.group_id'], methods=['GET', 'POST', 'DELETE','PUT'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]), results_per_page=0)
 
 # /api/app , /api/app/<int>
-manager.create_api(App, exclude_columns=['creator.password_hash'], methods=['GET', 'POST', 'DELETE','PUT'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+manager.create_api(App, exclude_columns=['creator.password_hash'], methods=['GET', 'POST', 'DELETE','PUT'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]), results_per_page=0)
 
 # /api/v2/app , /api/v2/app/<int>
-manager.create_api(App, include_columns=['name','link'], url_prefix='/api/v2', methods=['GET'])
+manager.create_api(App, include_columns=['name','link'], url_prefix='/api/v2', methods=['GET'], results_per_page=0)
 
 # /api/log , /api/log/<int>
-manager.create_api(Log, exclude_columns=['author.password_hash'], methods=['GET', 'POST'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
+manager.create_api(Log, exclude_columns=['author.password_hash'], methods=['GET', 'POST'], preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]), results_per_page=0)
 

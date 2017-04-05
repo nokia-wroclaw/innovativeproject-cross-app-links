7db3c8206c01e2f5f9ccc8f7b4bae07117c066b2
bakowroc <kontakt@maciejbakowicz.pl>
2017-04-05 17:24:01 +0200

    Add date timestamp filter

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -11,11 +11,15 @@
             scrollInertia: 0,
             axis: 'y'
         };
+
+
         $scope.limit = {
             users: 5,
             group: 3,
             log: 5
         }
+
+
         $scope.menu = {
             status: true,
             hide: function () {
@@ -141,6 +145,7 @@
         $scope.$on('$routeChangeStart', function (next, current) {
             $scope.newlink.clear();
             $scope.newlink.status = false;
+            $scope.searchBy = '';
         });
 
         //popup model

8872bd3c083c90288a2179bc4a5788e4abb9834e
bakowroc <kontakt@maciejbakowicz.pl>
2017-04-06 21:01:37 +0200

    Link validation, stylesheet fix

--- api/static/js/directives.js
+++ api/static/js/directives.js
@@ -5,7 +5,7 @@
             require: 'ngModel',
             link: function (scope, element, attr, mCtrl) {
                 function linkvalidate(value) {
-                    if (/^(http\:\/\/www+\.+[a-z0-9]+\.+[a-z0-9])$/.test(value)) {
+                    if (/^(htt+(p|ps)+\:\/\/www+\.+[a-z0-9]+\.+[a-z0-9])/.test(value)) {
                         mCtrl.$setValidity('linkformat', true);
                     } else {
                         mCtrl.$setValidity('linkformat', false);

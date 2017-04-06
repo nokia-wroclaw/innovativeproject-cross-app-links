31a17b5f8d17df076cd337facf965cb108d9c7ca
bakowroc <kontakt@maciejbakowicz.pl>
2017-04-06 21:34:53 +0200

    Fix media-q. reedit css, sort js functions

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -1,25 +1,14 @@
 (function () {
     var app = angular.module('mainApp', ['ngRoute', 'config', 'ngScrollbars', 'services', 'directives']);
     app.controller('mainCtrl', ['$scope', 'restful', '$location', '$routeParams', '$interval', function ($scope, restful, $location, $routeParams, $interval) {
-        /*Custom Scrollbar Config*/
-        $scope.config = {
-            autoHideScrollbar: true,
-            theme: 'minimal',
-            advanced: {
-                updateOnContentResize: true
-            },
-            scrollInertia: 0,
-            axis: 'y'
-        };
-
-
+        /*Limits for lists*/
         $scope.limit = {
             users: 5,
             group: 3,
             log: 5
         }
 
-
+        /*Menu elements*/
         $scope.menu = {
             status: true,
             hide: function () {
@@ -28,17 +17,16 @@
             location: function () {
                 return $location.path().replace(/\//g, '').replace(/\-/g, ' ');
             }
-        }
-
+        };
+        /*Filter apps by params in a route*/
         $scope.filterParams = {
             manage: function () {
                 if ($routeParams.linkID)
                     return parseInt($routeParams.linkID);
                 else return '';
             }
-        }
-        //Get data
-
+        };
+        /*Get data model*/
         var update = {
             me: function () {
                 restful.get('me/user').then(function (response) {
@@ -72,17 +60,18 @@
                 this.groups();
                 this.logs();
             }
-        }
+        };
         update.all();
 
-        //DASHBOARD COMPONENTS
+        /*Dashboard clock*/
         $scope.clockDate = {
             setup: new Date(),
             time: function () {
                 if (this.setup.getMinutes() < 10)
                     return this.setup.getHours() > 9 ? this.setup.getHours() + ':0' + this.setup.getMinutes() : '0' + this.setup.getHours() + ':0' + this.setup.getMinutes();
                 else
-                    return this.setup.getHours() > 9 ? this.setup.getHours() + ':' + this.setup.getMinutes() : '0' + this.setup.getHours() + ':' + this.setup.getMinutes();
+                    return this.setup.getHours() > 9 ? this.setup.getHours() + ':' + this.setup.getMinutes() : '0' + this.setup.getHours() + ':'
+                this.setup.getMinutes();
             },
             date: function () {
                 return this.setup.getTime();
@@ -90,13 +79,14 @@
             update: function () {
                 this.setup = new Date();
             }
-        }
+        };
+
+        /*Refresh clock every 30s*/
         $interval(function () {
             $scope.clockDate.update()
         }, 3000)
 
-        //ADDING ITEMS MODELS
-
+        /*Applications methods*/
         $scope.newlink = {
             name: '',
             address: '',
@@ -115,7 +105,6 @@
                 }
                 restful.post('app', post_object);
                 update.apps();
-
                 this.clear();
                 this.status = true;
             },
@@ -125,12 +114,11 @@
                     link: this.address,
                     desc: this.desc,
                 }
-
                 restful.update('app', app_id, post_object).then(function (response) {
                     var log_object = {
                         content: 'A link #' + app_id + ' was updated',
                         data_time: 'CURRENT_TIMESTAMP',
-                        author_id: 1
+                        author_id: $scope.current_user.id
                     }
                     restful.post('log', log_object);
                 });
@@ -141,15 +129,14 @@
                 this.status = true;
                 $location.path('/links').replace();
             },
-
             delete: function (app_id) {
                 var confirmResult = confirm("Do you want to remove this app?");
                 if (confirmResult) {
                     restful.delete("app", app_id).then(function (response) {
                         var log_object = {
-                            content: "A link #" + app_id + " was removed",
-                            data_time: "CURRENT_TIMESTAMP",
-                            author_id: 1
+                            content: 'A link #' + app_id + ' was removed',
+                            data_time: 'CURRENT_TIMESTAMP',
+                            author_id: $scope.current_user.id
                         }
                         restful.post("log", log_object);
                     });
@@ -157,7 +144,6 @@
                     update.logs();
                 }
             },
-
             clear: function () {
                 this.name = '';
                 this.address = '';
@@ -166,20 +152,48 @@
             status: false
         };
 
+        /*
+        If you use some variables in a couple places you probably 
+        want to reset them when you leave a page. That's why you should put
+        those variables/functions below.
+        */
         $scope.$on('$routeChangeStart', function (next, current) {
             $scope.newlink.clear();
             $scope.newlink.status = false;
             $scope.searchBy = '';
         });
 
-        //popup model
+        /*Popup model functions*/
         $scope.popup = {
             edit: {
                 close: function () {
                     $location.path('/links').replace();
                 }
             }
-        }
+        };
+        /*Some elements should change with resolution so it's nice to include them*/
+        var bootstrap_resolution = {
+            resl: document.body.innerWidth,
+            onchange: function () {
+
+                }
+                //1200
+                //768
+                //480
+                //320
+        };
+
+        /*Custom Scrollbar Config*/
+        $scope.config = {
+            autoHideScrollbar: true,
+            theme: 'minimal',
+            advanced: {
+                updateOnContentResize: true
+            },
+            scrollInertia: 0,
+            axis: 'y'
+        };
+
 
                     }]);
 

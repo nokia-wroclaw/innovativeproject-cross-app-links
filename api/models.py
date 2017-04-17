ad83ecec847cf5f39005bd57bf46cb27078f8f56
m_szwarc <lozohcum@gmail.com>
2017-04-17 15:28:29 +0200

    models: update

--- api/models.py
+++ api/models.py
@@ -10,21 +10,22 @@ flask_uuid.init_app(app)
 class User(UserMixin, db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
-    email=db.Column(db.String(25), unique=True)
-    username=db.Column(db.String(30), unique=True)
-    password_hash=db.Column(db.String(128))
-    group_id=db.Column(db.Integer, db.ForeignKey('group.id'))
+    email = db.Column(db.String(25), unique=True)
+    username = db.Column(db.String(30), unique=True)
+    password_hash = db.Column(db.String(128))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    date = db.Column(db.TIMESTAMP)
     
-    applications=db.relationship('App', backref='creator', lazy='dynamic')
-    logs=db.relationship('Log', backref='author', lazy='dynamic')
-    notes=db.relationship('Note', backref='owner', lazy='dynamic')
+    applications = db.relationship('App', backref='creator', lazy='dynamic')
+    logs = db.relationship('Log', backref='author', lazy='dynamic')
+    notes = db.relationship('Note', backref='owner', lazy='dynamic')
     
     def __init__(self, email, password_hash,group):
         
-        self.email=email
-        self.password_hash=password_hash
-        self.group_id=group
-        self.username=email.split('@')[0]
+        self.email = email
+        self.password_hash = password_hash
+        self.group_id = group
+        self.username = email.split('@')[0]
         
     def is_authenticated():
         return True
@@ -42,72 +43,74 @@ class User(UserMixin, db.Model):
             
 class Group(db.Model):
     
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(30), unique=True)
-    app_add=db.Column(db.Boolean)
-    app_edit=db.Column(db.Boolean)
-    app_drop=db.Column(db.Boolean)
-    user_add=db.Column(db.Boolean)
-    user_drop=db.Column(db.Boolean)
-    users=db.relationship('User', backref='group', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    app_add = db.Column(db.Boolean)
+    app_edit = db.Column(db.Boolean)
+    app_drop = db.Column(db.Boolean)
+    user_add = db.Column(db.Boolean)
+    user_drop = db.Column(db.Boolean)
+    users = db.relationship('User', backref='group', lazy='dynamic')
         
     def __init__(self, name, app_add, app_edit, app_drop, user_add, user_drop):
         
-        self.name=name
-        self.app_add=app_add
-        self.app_edit=app_edit
-        self.app_drop=app_drop
-        self.user_add=user_add
-        self.user_drop=user_drop
+        self.name = name
+        self.app_add = app_add
+        self.app_edit = app_edit
+        self.app_drop = app_drop
+        self.user_add = user_add
+        self.user_drop = user_drop
 
             
             
 class App(db.Model):
     
     id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(30), unique=True)
-    link=db.Column(db.String(50), unique=True)
-    desc=db.Column(db.String(50))
-    creator_id=db.Column(db.Integer, db.ForeignKey('user.id'))
-    img_link=db.Column(db.String(50))
-    order_id=db.Column(db.Integer)
-    status=db.Column(db.Boolean)
-    click_num=db.Column(db.Integer)
-    beta=db.Column(db.Boolean)
+    name = db.Column(db.String(30), unique=True)
+    link = db.Column(db.String(50), unique=True)
+    desc = db.Column(db.String(50))
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    img_link = db.Column(db.String(50))
+    order_id = db.Column(db.Integer)
+    status = db.Column(db.Boolean)
+    click_num = db.Column(db.Integer)
+    beta = db.Column(db.Boolean)
+    date = db.Column(db.TIMESTAMP)
 
 
     def __init__(self, name, link, desc, creator_id):
-        self.name=name
-        self.link=link
-        self.desc=desc
-        self.creator_id=creator_id
-        self.status= True
-        self.beta= False
+        self.name = name
+        self.link = link
+        self.desc = desc
+        self.creator_id = creator_id
+        self.status = True
+        self.beta = False
             
             
             
 class Log(db.Model):
     
-    id=db.Column(db.Integer,primary_key=True)
-    data_time=db.Column(db.TIMESTAMP)
-    content=db.Column(db.String(60),unique=True)
-    author_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer,primary_key=True)
+    data_time = db.Column(db.TIMESTAMP)
+    content = db.Column(db.String(60),unique=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, content, data_time, author_id):
         
-        self.content=content
-        self.data_time=data_time
-        self.author_id=author_id
+        self.content = content
+        self.data_time = data_time
+        self.author_id = author_id
         
 #To create
 
 class  Note(db.Model):
     __tablename__ = 'Note'
     
-    id=db.Column(db.Integer, primary_key=True)
-    content=db.Column(db.Text)
-    tag=db.Column(db.String(20))
-    owner_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    tag = db.Column(db.String(20))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date = db.Column(db.TIMESTAMP)
         
 #db.create_all()
 
@@ -115,17 +118,18 @@ class Invites(db.Model):
     __tablename__ = 'Invites'
     
     id = db.Column(db.Integer, primary_key=True)
-    email=db.Column(db.String(25), unique=True)
+    email = db.Column(db.String(25), unique=True)
     token = db.Column(db.String(50), unique=True)
     maker = db.Column(db.Integer)
     group = db.Column(db.Integer)
+    date = db.Column(db.TIMESTAMP)
     
     def __init__(self, email, maker,group):
         
-        self.email=email
-        self.maker=maker
-        self.group=group
-        self.token=str(uuid.uuid4())
+        self.email = email
+        self.maker = maker
+        self.group = group
+        self.token = str(uuid.uuid4())
 
 
 class Stats(db.Model):
@@ -140,9 +144,9 @@ class Stats(db.Model):
 
     def __init__(self, timestamp,users,apps.clicks)
 
-    self.timestamp=timestamp
-    self.users=users
-    self.apps=apps
-    self.clicks=clicks
+    self.timestamp = timestamp
+    self.users = users
+    self.apps = apps
+    self.clicks = clicks
 
 

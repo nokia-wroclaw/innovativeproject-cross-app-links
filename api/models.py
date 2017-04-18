2a9f9aacc9b4ae35bb7764990b1b395351887052
m_szwarc <lozohcum@gmail.com>
2017-04-18 23:13:56 +0200

    models: temp. remove date

--- api/models.py
+++ api/models.py
@@ -20,13 +20,13 @@ class User(UserMixin, db.Model):
     logs = db.relationship('Log', backref='author', lazy='dynamic')
     notes = db.relationship('Note', backref='owner', lazy='dynamic')
     
-    def __init__(self, email, password_hash,group,date):
+    def __init__(self, email, password_hash,group):
         
         self.email = email
         self.password_hash = password_hash
         self.group_id = group
         self.username = email.split('@')[0]
-        self.date = date
+#        self.date = date
         
     def is_authenticated():
         return True
@@ -132,13 +132,13 @@ class Invites(db.Model):
     group = db.Column(db.Integer)
     date = db.Column(db.TIMESTAMP)
     
-    def __init__(self, email, maker,group,date):
+    def __init__(self, email, maker,group):
         
         self.email = email
         self.maker = maker
         self.group = group
         self.token = str(uuid.uuid4())
-        self.date = date
+#        self.date = date
 
 
 class Reset(db.Model):

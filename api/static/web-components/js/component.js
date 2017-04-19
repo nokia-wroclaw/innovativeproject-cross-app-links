69bec1e6d541a3ee5b6c27e3ea039533c065f80e
bakowroc <kontakt@maciejbakowicz.pl>
2017-04-19 18:45:28 +0200

    Create iron-ajax in template for data fetching

--- api/static/web-components/js/component.js
+++ api/static/web-components/js/component.js
@@ -16,29 +16,6 @@
                 componentOverflow.className = componentOverflow.className.replace(/visible/g, 'c-hidden');
             }
         },
-        request: function () {
-
-            var req = new XMLHttpRequest();
-            // creating GET request to yahoo weather API
-            req.open('GET', "/api/v2/app", true);
-            // sending a request
-            req.send();
-
-            req.onreadystatechange = generateHTML;
-            var navbar = document.querySelector('.component-overflow .component-ul');
-
-            function generateHTML() {
-                if (req.readyState == 4 && req.status == 200) {
-                    var listGenerateString = '';
-                    var linkArray = JSON.parse(req.responseText);
-                    linkArray = linkArray['objects'];
-                    for (var i = 0; i < linkArray.length; i++) {
-                        listGenerateString += '<li><a href="' + linkArray[i]['link'] + '"> <div class="option-icon"><span class="glyphicon glyphicon-option-horizontal"></span></div><img src="/static/img/app-img/' + linkArray[i]['img_link'] + '.png" /><span class="text">' + linkArray[i]['name'] + '</span></a></li>';
-                    }
-                    navbar.insertAdjacentHTML('beforeend', listGenerateString);
-                }
-            }
-        },
         start: function () {
             var button = this.button;
             var component = this.element;
@@ -52,7 +29,6 @@
             componentOverflow.style.top = parseInt(pageNavbarHeight) + 'px';
             componentOverflow.style.paddingBottom = parseInt(pageNavbarHeight) + 'px';
             button.addEventListener('click', this.use.bind(this));
-            this.request();
             component.style.visibility = 'visible';
         }
     };
